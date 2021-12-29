b5f387960120c609f670a11559b142787e205389
tfreakygeek <rsureshkumar304@gmail.com>
2021-12-29 19:46:01 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/technosoftwares/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.technosoftwares
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/planspage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlansPage extends StatefulWidget {
+  @override
+  _PlansPageState createState() => _PlansPageState();
+}
+
+class _PlansPageState extends State<PlansPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
