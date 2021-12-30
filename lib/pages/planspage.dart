4ce09b8c4f203999a8b727484aa4e28bf15fda14
tfreakygeek <rsureshkumar304@gmail.com>
2021-12-30 12:32:46 +0530

    Plans page Front end Done

--- lib/pages/planspage.dart
+++ lib/pages/planspage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PlansPage extends StatefulWidget {
   @override
@@ -8,12 +9,90 @@ class PlansPage extends StatefulWidget {
 class _PlansPageState extends State<PlansPage> {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: Text(
-          "PlansPage",
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).padding.top + 0.15 * height,
+            ),
+            Image.asset("assets/pickplan.png"),
+            SizedBox(
+              height: 30,
+            ),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: "Subscribe a Plan",
+                    style: GoogleFonts.assistant(
+                      textStyle: TextStyle(
+                        color: Colors.grey[800],
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                        "\n\nSubscribe a plan to change your vehicle at your home or office",
+                    style: GoogleFonts.assistant(
+                      textStyle: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            OutlinedButton(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Text(
+                  "BROWSE",
+                  style: TextStyle(
+                    color: Colors.green,
+                    letterSpacing: 3,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                  side: BorderSide(
+                    width: 1.0,
+                    color: Colors.grey[400],
+                  ),
+                  shape: StadiumBorder()),
+            ),
+          ],
         ),
       ),
+      // body: Container(
+      //   child: Center(
+      //     child: Text(
+      //       "Subscribe a Plan",
+      //       style: GoogleFonts.assistant(
+      //         textStyle: TextStyle(
+      //           color: Colors.grey[800],
+      //           fontSize: 25,
+      //           fontWeight: FontWeight.bold,
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
